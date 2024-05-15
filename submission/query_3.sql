fa298e1e4f1c7696d5966423833d2ebbdbabe3b4
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-15 18:42:54 -0400

    Update query_3.sql

---
+++ submission/query_3.sql
@@ -0,0 +1,22 @@
+/*
+This table tracks both quality_class and is_active dimensions with the same start_date and end_date
+*/
+CREATE OR REPLACE TABLE actors_history_scd (
+  -- actor: Full name of the actor.
+  actor VARCHAR,
+  -- quality_class: Category based on the Average rating for a given start_date and end_date.
+  quality_class VARCHAR,
+  -- is_active: Whether the actor is active and making the films for a given start_date and end_date.
+  is_active BOOLEAN,
+  -- start_date: Start date to track the two dimensions.
+  start_date INTEGER,
+  -- end_date: End date to track the two dimensions.
+  end_date INTEGER,
+  -- current_year: Represents the most recent year.
+  current_year INTEGER
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    PARTITIONING = ARRAY['current_year']
+  )
