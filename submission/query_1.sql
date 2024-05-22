f9dc7642af94f5dfc8bd159159aad7078e94c6c9
Nikhil Sahni <54640886+nickable90@users.noreply.github.com>
2024-05-21 23:07:52 -0400

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,7 +1,7 @@
 /*
 This is to create a cumulative design table, where we maintain the history of changing dimensions in a single column 
 as an array of arrays(or rows) in a single record. So that the latest partition or record can be used to pull the entire history.
-"films" in this table is an array that maintains rows of all the changing dimensions from different years.
+"films" in this table is an array that maintains rows of all the changing dimensions from different years
 */
 CREATE OR REPLACE TABLE nikhilsahni.actors (
   -- actor: Actor's full name.
